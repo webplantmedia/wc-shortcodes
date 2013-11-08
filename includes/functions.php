8afb4422475b3e6fcebf17204abfad726358117a
Chris Baldelomar <chris@webplantmedia.com>
2013-11-07 19:07:59 -0600

    updated email to filter

--- includes/functions.php
+++ includes/functions.php
@@ -10,8 +10,10 @@ function wc_shortcodes_smart_social_link( $social_link, $name ) {
     switch ( $name ) {
         case 'email' :
             // some users may have already inserted mailto:, so let's remove it.
-            $social_link = str_replace( 'mailto:', '', $social_link );
-            $social_link = 'mailto:'.$social_link;
+			if ( is_email( $social_link ) ) {
+				$social_link = str_replace( 'mailto:', '', $social_link );
+				$social_link = 'mailto:'.$social_link;
+			}
             break;
     }
 
