040b76022140390676a34565691eec6fa344992e
Chris Baldelomar <chris@webplantmedia.com>
2014-01-30 22:10:26 -0600

    Appended version number to JS file so new scripts get loaded.

--- includes/mce/shortcodes_tinymce.php
+++ includes/mce/shortcodes_tinymce.php
@@ -1,17 +1,4 @@
 <?php
-/**
- * This file has all the main shortcode functions
- * @package wc Shortcodes Plugin
- * @since 1.0
- * @author AJ Clarke : http://wpexplorer.com
- * @copyright Copyright (c) 2012, AJ Clarke
- * @link http://wpexplorer.com
- * @License: GNU General Public License version 3.0
- * @License URI: http://www.gnu.org/licenses/gpl-3.0.html
- *
- *
- * Special thank you to my buddy Syamil @ http://aquagraphite.com/
- */
 class WC_Shortcodes_TinyMCE_Buttons {
 	function __construct() {
     	add_action( 'init', array(&$this,'init') );
@@ -19,18 +6,20 @@ class WC_Shortcodes_TinyMCE_Buttons {
     function init() {
 		if ( ! current_user_can('edit_posts') && ! current_user_can('edit_pages') )
 			return;		
+
 		if ( get_user_option('rich_editing') == 'true' ) {  
 			add_filter( 'mce_external_plugins', array(&$this, 'add_plugin') );  
 			add_filter( 'mce_buttons', array(&$this,'register_button') ); 
 		}  
     }  
 	function add_plugin($plugin_array) {  
-	   $plugin_array['wc_shortcodes'] = plugin_dir_url( __FILE__ ) .'js/shortcodes_tinymce.js';
-	   return $plugin_array; 
+		$ver = WC_SHORTCODES_VERSION;
+		$plugin_array['wc_shortcodes'] = plugin_dir_url( __FILE__ ) .'js/shortcodes_tinymce.js?ver=' . $ver;
+		return $plugin_array; 
 	}
 	function register_button($buttons) {  
-	   array_push($buttons, "wc_shortcodes_button");
-	   return $buttons; 
+		array_push($buttons, "wc_shortcodes_button");
+		return $buttons; 
 	} 	
 }
 $wcshortcode = new WC_Shortcodes_TinyMCE_Buttons;
