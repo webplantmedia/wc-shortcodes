e299e0e93bb9af7b54b95c461482ebf8daa432aa
Chris Baldelomar <chris@webplantmedia.com>
2014-03-07 16:13:09 -0600

    Fixed bug with isotope pagination not working on homepage.

--- includes/templates/nav-pagination.php
+++ includes/templates/nav-pagination.php
@@ -12,7 +12,7 @@ if ( $ml_query->max_num_pages < 2 )
 				'base' => str_replace( $big, '%#%', esc_url( get_pagenum_link( $big ) ) ),
 				'format' => '?page=%#%', // ?page=%#% : %#% is replaced by the page number
 				'total' => $ml_query->max_num_pages,
-				'current' => max( 1, get_query_var('paged') ),
+				'current' => max( 1, $ml_query->get('paged') ),
 				'show_all' => false,
 				'prev_next' => true,
 				'prev_text' => __('Previous Page'),
