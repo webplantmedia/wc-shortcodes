1876b6a6e41f972b6e11e1bbabea34a906dd5d5a
Chris Baldelomar <chris@webplantmedia.com>
2014-06-10 14:51:24 -0500

    Improved social icons widget

--- includes/widgets.php
+++ includes/widgets.php
@@ -36,25 +36,39 @@ class WC_Shortcodes_Social_Icons_Widget extends WP_Widget {
 			echo $args['before_title'] . $instance['title'] . $args['after_title'];
 
 		// set class with the number of columns the user selected
-		$columns = (int) $instance['columns'];
+		$columns = $instance['columns'];
 		if ( empty($columns) ) {
-			$columns = 3;
+			$columns = 'float-left';
+		}
+		$maxheight = 'none';
+		if ( isset( $instance['maxheight'] ) ) {
+			$maxheight = $instance['maxheight'];
 		}
 
 		$order = $instance['order'];
 		$first = true;
 
-		$class = ' wc-shortcodes-columns-'.$columns;
+		$column_display = false;
+		if ( is_numeric( $columns ) & (int) $columns > 0 ) {
+			$column_display = true;
+		}
 
-		$html = '<ul class="wc-shortcodes-social-icons wc-shortcodes-clearfix'.$class.'">';
+		$classes = array();
+		$classes[] = 'wc-shortcodes-social-icons';
+		$classes[] = 'wc-shortcodes-clearfix';
+		$classes[] = 'wc-shortcodes-columns-'.$columns;
+		$classes[] = 'wc-shortcodes-maxheight-'.$maxheight;
+
+		$html = '<ul class="'.implode( ' ', $classes ).'">';
 			$i = 0;
 			foreach ($order as $key => $name) {
 				$li_class = array();
 				$li_class[] = 'wc-shortcodes-social-icon';
 				$li_class[] = 'wc-shortcode-social-icon-' . $key;
 
-				if ( $i % $columns == 0 ) 
+				if ( $column_display && $i % $columns == 0 ) {
 					$li_class[] = 'clear-left';
+				}
 
 				$link_option_name = WC_SHORTCODES_PREFIX . $key . '_link';
 				$icon_option_name = WC_SHORTCODES_PREFIX . $key . '_icon';
@@ -87,7 +101,8 @@ class WC_Shortcodes_Social_Icons_Widget extends WP_Widget {
 
 	function update( $new_instance, $old_instance ) {
 		$instance['title'] = strip_tags( stripslashes($new_instance['title']) );
-		$instance['columns'] = (int) $new_instance['columns'];
+		$instance['columns'] = $new_instance['columns'];
+		$instance['maxheight'] = $new_instance['maxheight'];
 		$instance['order'] = $new_instance['order'];
 		return $instance;
 	}
@@ -111,8 +126,13 @@ class WC_Shortcodes_Social_Icons_Widget extends WP_Widget {
 		);
 		$order = isset( $instance['order'] ) ? $instance['order'] : $default_order;
 		$title = isset( $instance['title'] ) ? $instance['title'] : 'Follow Me!';
-		$columns = isset( $instance['columns'] ) ? (int) $instance['columns'] : 6;
+		$columns = isset( $instance['columns'] ) ? $instance['columns'] : 6;
+		$maxheight = isset( $instance['maxheight'] ) ? $instance['maxheight'] : 'none';
 		?>
+		<p>
+			<label for="<?php echo $this->get_field_id('title'); ?>"><?php _e('Title:') ?></label>
+			<input type="text" class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" value="<?php echo $title; ?>" />
+		</p>
 		<label><?php _e('Order:'); ?></label>
 		<ul class="wc-shortcodes-clearfix wc-shortcodes-social-icons">
 			<?php foreach ( $order as $key => $name ) : ?>
@@ -123,20 +143,28 @@ class WC_Shortcodes_Social_Icons_Widget extends WP_Widget {
 			<?php endforeach; ?>
 		</ul>
 		<p>
-			<label for="<?php echo $this->get_field_id('title'); ?>"><?php _e('Title:') ?></label>
-			<input type="text" class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" value="<?php echo $title; ?>" />
+			<label for="<?php echo $this->get_field_id('columns'); ?>"><?php _e('Display:'); ?></label>
+			<select id="<?php echo $this->get_field_id('columns'); ?>" name="<?php echo $this->get_field_name('columns'); ?>">
+				<option value="float-center"<?php selected( $columns, 'float-center' ); ?>>Float Center</option>';
+				<option value="float-left"<?php selected( $columns, 'float-left' ); ?>>Float Left</option>';
+				<option value="float-right"<?php selected( $columns, 'float-right' ); ?>>Float Right</option>';
+				<option value="1"<?php selected( $columns, '1' ); ?>>1 Column</option>';
+				<option value="2"<?php selected( $columns, '2' ); ?>>2 Columns</option>';
+				<option value="3"<?php selected( $columns, '3' ); ?>>3 Columns</option>';
+				<option value="4"<?php selected( $columns, '4' ); ?>>4 Columns</option>';
+				<option value="5"<?php selected( $columns, '5' ); ?>>5 Columns</option>';
+				<option value="6"<?php selected( $columns, '6' ); ?>>6 Columns</option>';
+				<option value="7"<?php selected( $columns, '7' ); ?>>7 Columns</option>';
+				<option value="8"<?php selected( $columns, '8' ); ?>>8 Columns</option>';
+			</select>
 		</p>
 		<p>
-			<label for="<?php echo $this->get_field_id('columns'); ?>"><?php _e('Number of Columns:'); ?></label>
-			<select id="<?php echo $this->get_field_id('columns'); ?>" name="<?php echo $this->get_field_name('columns'); ?>">
-				<option value="1"<?php selected( $columns, '1' ); ?>>1</option>';
-				<option value="2"<?php selected( $columns, '2' ); ?>>2</option>';
-				<option value="3"<?php selected( $columns, '3' ); ?>>3</option>';
-				<option value="4"<?php selected( $columns, '4' ); ?>>4</option>';
-				<option value="5"<?php selected( $columns, '5' ); ?>>5</option>';
-				<option value="6"<?php selected( $columns, '6' ); ?>>6</option>';
-				<option value="7"<?php selected( $columns, '7' ); ?>>7</option>';
-				<option value="8"<?php selected( $columns, '8' ); ?>>8</option>';
+			<label for="<?php echo $this->get_field_id('maxheight'); ?>"><?php _e('Max Height:'); ?></label>
+			<select id="<?php echo $this->get_field_id('maxheight'); ?>" name="<?php echo $this->get_field_name('maxheight'); ?>">
+				<option value="none"<?php selected( $maxheight, 'none' ); ?>>None</option>';
+				<?php for( $i = 10; $i <= 70; $i = $i + 2 ) : ?>
+					<option value="<?php echo $i; ?>"<?php selected( $maxheight, $i ); ?>><?php echo $i; ?>px</option>';
+				<?php endfor; ?>
 			</select>
 		</p>
 		<script type="text/javascript">
