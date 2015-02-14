2f78b8aec61421ad5f3399e65329121206d966e1
Chris Baldelomar <chris@webplantmedia.com>
2015-02-13 19:53:24 -0600

    Removed namespace which is not supported in php 5.2

--- includes/vendors/wpc-settings-framework/class-wpc-settings-framework.php
+++ includes/vendors/wpc-settings-framework/class-wpc-settings-framework.php
@@ -1,6 +1,4 @@
 <?php
-namespace WC_Shortcodes;
-
 /**
  * WPC Settings Framework.
  *
@@ -17,7 +15,7 @@ namespace WC_Shortcodes;
  * @package WPC_Settings_Framework
  * @author  Chris Baldelomar <chris@webplantmedia.com>
  */
-class WPC_Settings_Framework {
+class WC_Shortcodes_Settings_Framework {
 
 	protected $version = '1.0.0';
 
@@ -66,9 +64,6 @@ class WPC_Settings_Framework {
 	 */
 	private function __construct() {
 
-		require_once( 'class-wpc-settings-framework-sanitize.php' );
-		$this->sanitize = WPC_Settings_Framework_Sanitize::get_instance();
-
 		$this->set_slug_prefix();
 
 		add_action( 'admin_init', array( $this, 'set_plugin_info' ) );
@@ -107,8 +102,8 @@ class WPC_Settings_Framework {
 			return;
 		}
 
-		$this->plugin_slug = $this->sanitize->id( $plugin_name );
-		$this->plugin_prefix = $this->sanitize->key( $plugin_name ) . '_';
+		$this->plugin_slug = $this->sanitize_id( $plugin_name );
+		$this->plugin_prefix = $this->sanitize_key( $plugin_name ) . '_';
 	}
 	
 	public function set_plugin_info() {
@@ -289,8 +284,8 @@ class WPC_Settings_Framework {
 		}
 
 		$callback = array(
-			$this->sanitize,
-			$this->sanitize->callback( $o['type'] ),
+			$this,
+			$this->sanitize_callback( $o['type'] ),
 		);
 
 		return $callback;
@@ -522,5 +517,596 @@ class WPC_Settings_Framework {
 				require( 'views/settings/input-field.php' );
 				break;
 		}
+	}
+
+	/************
+	 * Sanitize *
+	 ************/
+
+	/**
+	 * Given an option type, we will return a string
+	 * of the callback function used to sanitize
+	 * the option value
+	 *
+	 * @since 3.5.2
+	 * @access public
+	 *
+	 * @param string $type 
+	 * @return string
+	 */
+	public function sanitize_callback( $type ) {
+		switch ( $type ) {
+			case 'color' :
+				return 'sanitize_hex_color';
+			case 'image' :
+				return 'sanitize_esc_url_raw';
+			case 'positive_pixel' :
+				return 'sanitize_positive_pixel';
+			case 'pixel' :
+				return 'sanitize_pixel';
+			case 'number' :
+				return 'sanitize_number';
+			case 'decimal' :
+				return 'sanitize_decimal';
+			case 'border' :
+				return 'sanitize_border';
+			case 'background' :
+				return 'sanitize_background_css';
+			case 'checkbox' :
+				return 'sanitize_checkbox';
+			case 'gallery' :
+				return 'sanitize_gallery';
+			case 'emails' :
+				return 'sanitize_emails';
+		}
+
+		return 'sanitize_none';
+	}
+
+	/**
+	 * Validate slideshow data saved to database.
+	 *
+	 * @since 3.6
+	 * @access public
+	 *
+	 * @param array $value 
+	 * @return array
+	 */
+	public function sanitize_gallery( $value ) {
+		if ( empty( $value ) )
+			return null;
+
+		$ids = explode( ',', $value );
+
+		foreach ( $ids as $key => $id ) {
+			if ( ! is_numeric( $id ) ) {
+				unset( $ids[ $key ] );
+			}
+		}
+
+		$value = implode( ',', $ids );
+
+		return $value;
+	}
+
+	/**
+	 * Sanitize border values. Border consists
+	 * of pixel value, border style, and color.
+	 *
+	 * @since 3.6
+	 * @access public
+	 *
+	 * @param array $value 
+	 * @return array
+	 */
+	public function sanitize_border( $value ) {
+		$border = array(
+			'width' => '0px',
+			'style' => 'none',
+			'color' => '#ffffff',
+		);
+
+		if ( ! is_array( $value ) )
+			return $border;
+
+		foreach ( $value as $k => $v ) {
+			switch ( $k ) {
+				case 'width' :
+					$v = $this->sanitize_positive_pixel( $v );
+					$border['width'] = $v;
+					break;
+				case 'style' :
+					$v = $this->sanitize_border_style( $v );
+					$border['style'] = $v;
+					break;
+				case 'color' :
+					$v = $this->sanitize_hex_color( $v );
+					$border['color'] = $v;
+					break;
+			}
+		}
+
+		return $border;
+	}
+
+	/**
+	 * Strips all non numerica characters and returns
+	 * intval() of string. Only allows for positive values.
+	 *
+	 * @since 3.6
+	 * @access public
+	 *
+	 * @param string $value 
+	 * @return void
+	 */
+	public function sanitize_positive_pixel( $value ) {
+		$value = preg_replace("/[^0-9]/", "",$value);
+		$value = intval( $value );
+
+		if ( empty( $value ) )
+			$value = '0';
+
+		return $value."px";
+	}
+
+	/**
+	 * Strips all non numerica characters and returns
+	 * intval() of string. Allows both negative and
+	 * positive values.
+	 *
+	 * @since 3.6
+	 * @access public
+	 *
+	 * @param string $value 
+	 * @return void
+	 */
+	public function sanitize_pixel( $value ) {
+		$value = preg_replace("/[^0-9\-]/", "",$value);
+		$value = intval( $value );
+
+		if ( empty( $value ) )
+			$value = '0';
+
+		return $value."px";
+	}
+
+	public function sanitize_font( $value ) {
+		$font = array(
+			'font_family' => '',
+			'font_size' => '',
+			'text_transform' => '',
+			'font_style' => '',
+			'font_weight' => '',
+			'color' => '',
+		);
+
+		if ( !is_array( $value ) )
+			return $font;
+
+		foreach ( $value as $k => $v ) {
+			switch ( $k ) {
+				case 'font_family' :
+					$font['font_family'] = $v;
+					break;
+				case 'font_size' :
+					$v = $this->sanitize_pixel( $value['font_size'] );
+					$font['font_size'] = $v;
+					break;
+				case 'text_transform' :
+					$font['text_transform'] = $v;
+					break;
+				case 'font_style' :
+					$v = $this->sanitize_font_style( $v );
+					$font['font_style'] = $v;
+					break;
+				case 'font_weight' :
+					$v = $this->sanitize_font_weight( $v );
+					$font['font_weight'] = $v;
+					break;
+				case 'color' :
+					$v = $this->sanitize_hex_color( $v );
+					$font['color'] = $v;
+					break;
+			}
+		}
+
+		return $font;
+	}
+
+	public function sanitize_font_hover( $value ) {
+		$font = array(
+			'text_decoration' => '',
+			'color' => '',
+		);
+
+		if ( !is_array( $value ) )
+			return $font;
+
+		foreach ( $value as $k => $v ) {
+			switch ( $k ) {
+				case 'text_decoration' :
+					$v = $this->sanitize_text_decoration( $v );
+					$font['text_decoration'] = $v;
+					break;
+				case 'color' :
+					$v = $this->sanitize_hex_color( $v );
+					$font['color'] = $v;
+					break;
+			}
+		}
+
+		return $font;
+	}
+
+	public function sanitize_font_appearance( $value ) {
+		$font = array(
+			'text_decoration' => '',
+			'font_style' => '',
+			'font_weight' => '',
+			'color' => '',
+		);
+
+		if ( !is_array( $value ) )
+			return $font;
+
+		foreach ( $value as $k => $v ) {
+			switch ( $k ) {
+				case 'text_decoration' :
+					$v = $this->sanitize_text_decoration( $v );
+					$font['text_decoration'] = $v;
+					break;
+				case 'font_style' :
+					$v = $this->sanitize_font_style( $v );
+					$font['font_style'] = $v;
+					break;
+				case 'font_weight' :
+					$v = $this->sanitize_font_weight( $v );
+					$font['font_weight'] = $v;
+					break;
+				case 'color' :
+					$v = $this->sanitize_hex_color( $v );
+					$font['color'] = $v;
+					break;
+			}
+		}
+
+		return $font;
+	}
+
+	public function sanitize_text_decoration( $value ) {
+		$whitelist = array(
+			'none',
+			'underline',
+			'overline',
+			'line-through',
+		);
+
+		if ( in_array( $value, $whitelist ) )
+			return $value;
+
+		return '';
+	}
+
+	public function sanitize_text_transform( $value ) {
+		$whitelist = array(
+			'none',
+			'capitalize',
+			'uppercase',
+			'lowercase',
+		);
+
+		if ( in_array( $value, $whitelist ) )
+			return $value;
+
+		return '';
+	}
+
+	public function sanitize_font_style( $value ) {
+		$whitelist = array(
+			'normal',
+			'italic',
+			'oblique',
+		);
+
+		if ( in_array( $value, $whitelist ) )
+			return $value;
+
+		return '';
+	}
+
+	public function sanitize_font_weight( $value ) {
+		$whitelist = array(
+			'normal',
+			'bold',
+			'bolder',
+			'lighter',
+			'100',
+			'200',
+			'300',
+			'400',
+			'500',
+			'600',
+			'700',
+			'800',
+			'900',
+		);
+
+		if ( in_array( $value, $whitelist ) )
+			return $value;
+
+		return '';
+	}
+
+	public function sanitize_background_css( $value ) {
+		$background = array(
+			'color' => '',
+			'image' => '',
+			'repeat' => '',
+			'position' => '',
+			'attachment' => '',
+		);
+
+		if ( !is_array( $value ) )
+			return $background;
+
+		foreach ( $value as $k => $v ) {
+			switch ( $k ) {
+				case 'color' :
+					$v = $this->sanitize_hex_color( $v );
+					$background['color'] = $v;
+					break;
+				case 'image' :
+					$v = esc_url_raw( $v );
+					$background['image'] = $v;
+					break;
+				case 'repeat' :
+					$v = $this->sanitize_background_repeat( $v );
+					$background['repeat'] = $v;
+					break;
+				case 'position' :
+					$v = $this->sanitize_background_position( $v );
+					$background['position'] = $v;
+					break;
+				case 'attachment' :
+					$v = $this->sanitize_background_attachment( $v );
+					$background['attachment'] = $v;
+					break;
+			}
+		}
+
+		return $background;
+	}
+
+	public function sanitize_background_repeat( $value ) {
+		$whitelist = array( 'repeat', 'no-repeat', 'repeat-x', 'repeat-y' );
+
+		if ( in_array( $value, $whitelist ) )
+			return $value;
+
+		return '';
+	}
+
+	public function sanitize_border_style( $value ) {
+		$whitelist = array(
+			'none',
+			'hidden',
+			'dotted',
+			'dashed',
+			'solid',
+			'double',
+			'groove',
+			'ridge',
+			'inset',
+			'outset',
+			'inherit',
+		);
+
+		if ( in_array( $value, $whitelist ) )
+			return $value;
+
+		return 'none';
+	}
+
+	public function sanitize_background_position( $value ) {
+		$whitelist = array(
+			'left top',
+			'left center',
+			'left bottom',
+			'right top',
+			'right center',
+			'right bottom',
+			'center top',
+			'center center',
+			'center bottom',
+		);
+
+		if ( in_array( $value, $whitelist ) )
+			return $value;
+
+		return '';
+	}
+
+	public function sanitize_background_attachment( $value ) {
+		$whitelist = array( 'fixed', 'scroll' );
+
+		if ( in_array( $value, $whitelist ) )
+			return $value;
+
+		return '';
+	}
+
+	public function sanitize_hex_color( $color ) {
+		if ( '' === $color )
+			return '';
+
+		if ( preg_match('|^#([A-Fa-f0-9]{3}){1,2}$|', $color ) )
+			return $color;
+
+		return null;
+	}
+
+	/**
+	 * replace nonalphannumeric charachers with underscore
+	 * should be safe enought to use as array key
+	 *
+	 * @since 3.5.2
+	 * @access public
+	 *
+	 * @param string $key 
+	 * @return string
+	 */
+	public function sanitize_key( $key ) {
+		$key = strtolower( preg_replace( '/[^a-zA-Z0-9]/', '_', $key ) );
+
+		return $key;
+	}
+
+	/**
+	 * replace nonalphannumeric charachers with hyphen
+	 * should be safe enough to use as a CSS id
+	 *
+	 * @since 3.5.2
+	 * @access public
+	 *
+	 * @param string $key 
+	 * @return string
+	 */
+	public function sanitize_id( $id ) {
+		$id = strtolower( preg_replace( '/[^a-zA-Z0-9]/', '-', $id ) );
+
+		return $id;
+	}
+
+	/**
+	 * return numeric values only
+	 *
+	 * @since 3.6
+	 * @access public
+	 *
+	 * @param string $number 
+	 * @return int
+	 */
+	public function sanitize_number( $number ) {
+		$number = (int) preg_replace( "/[^0-9\-]/", "", $number );
+
+		return $number;
+	}
+
+	/**
+	 * return decimal number
+	 *
+	 * @since 3.6.1
+	 * @access public
+	 *
+	 * @param mixed $number
+	 * @return void
+	 */
+	public function sanitize_decimal( $number ) {
+		$number = preg_replace( "/[^0-9\.\-]/", "", $number );
+
+		return $number;
+	}
+
+	/**
+	 * replace space with plus sign. Should be safe enough 
+	 * to use in Google Font stylesheet link
+	 *
+	 * @since 3.5.2
+	 * @access public
+	 *
+	 * @param string $code 
+	 * @return string
+	 */
+	public function sanitize_google_code( $code ) {
+		$code = preg_replace( '/\s/', '+', $code );
+
+		return $code;
+	}
+
+	/**
+	 * Parse only friendly characters to use in family name
+	 * inside css file.
+	 *
+	 * @since 3.5.2
+	 * @access public
+	 *
+	 * @param string $name 
+	 * @return string
+	 */
+	public function sanitize_font_family_name( $name ) {
+		$name = preg_replace( '/[^a-zA-Z0-9\-_]/', '', $name );
+
+		return $name;
+	}
+
+	/**
+	 * Checkbox should only return 1 or 0
+	 *
+	 * @since 3.5.2
+	 * @access public
+	 *
+	 * @param string $val 
+	 * @return void
+	 */
+	public function sanitize_checkbox( $val ) {
+		if ( $val )
+			return 1;
+		else
+			return 0;
+	}
+
+	/**
+	 * Make sure sidebar is valid
+	 *
+	 * @since 3.6.1
+	 * @access public
+	 *
+	 * @param mixed $value
+	 * @return void
+	 */
+	public function sanitize_sidebar( $value ) {
+		global $wp_registered_sidebars;
+
+		if ( 'none' == $value )
+			return $value;
+
+		if ( array_key_exists( $value, $wp_registered_sidebars ) )
+			return $value;
+
+		return 'none';
+	}
+
+	/**
+	 * Sanitize multiple emails
+	 *
+	 * @since 3.7.1
+	 * @access public
+	 *
+	 * @param mixed $email
+	 * @return void
+	 */
+	public function sanitize_emails( $email ) {
+		$valid = array();
+
+		$email = explode( ',', $email );
+
+		foreach ( $email as $e ) {
+			$e = trim( $e );
+			if ( is_email( $e ) )
+				$valid[] = $e;
+		}
+
+		if ( ! empty( $valid ) )
+			return implode( ',', $valid );
+
+		return null;
+	}
+
+	public function sanitize_esc_url_raw( $value ) {
+		return esc_url_raw( $value );
+	}
+	
+	public function sanitize_none( $value ) {
+		return $value;
 	}
 }
--- includes/vendors/wpc-settings-framework/init.php
+++ includes/vendors/wpc-settings-framework/init.php
@@ -9,5 +9,5 @@ if ( is_admin() && ( ! defined( 'DOING_AJAX' ) || ! DOING_AJAX ) ) {
 
 	require_once( 'class-wpc-settings-framework.php' );
 
-	add_action( 'plugins_loaded', array( 'WC_Shortcodes\\WPC_Settings_Framework', 'get_instance' ) );
+	add_action( 'plugins_loaded', array( 'WC_Shortcodes_Settings_Framework', 'get_instance' ) );
 }
