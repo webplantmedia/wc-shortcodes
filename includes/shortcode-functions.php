fc56a1562f2378ca5848e3682c31c2cc7bb0244d
Chris Baldelomar <chris@webplantmedia.com>
2015-10-23 15:40:33 -0500

    changed urlencode to rawurlencode

--- includes/shortcode-functions.php
+++ includes/shortcode-functions.php
@@ -1528,7 +1528,7 @@ if ( ! function_exists('wc_shortcodes_share_buttons') ) {
 							break;
 						case 'facebook' :
 							$html .= '<li class="wc-shortcodes-share-button-icon wc-shortcode-share-button-icon-' . $key . $first_class . '">';
-								$html .='<a target="_blank" href="http://www.facebook.com/sharer/sharer.php?u='.get_permalink().'&amp;t='.urlencode(get_the_title()).'">';
+								$html .='<a target="_blank" href="http://www.facebook.com/sharer/sharer.php?u='.get_permalink().'&amp;t='.rawurlencode(get_the_title()).'">';
 									switch ( $format ) {
 										case 'image' :
 											$html .= '<img src="'.$icon_url.'" alt="'.$icon_text.'">';
@@ -1545,7 +1545,7 @@ if ( ! function_exists('wc_shortcodes_share_buttons') ) {
 							break;
 						case 'twitter' :
 							$html .= '<li class="wc-shortcodes-share-button-icon wc-shortcode-share-button-icon-' . $key . $first_class . '">';
-								$html .='<a target="_blank" onclick="return !window.open(this.href, \'Twitter\', \'width=500,height=430\')" href="https://twitter.com/share?text='.urlencode(get_the_title()).'&amp;url='.get_permalink().'" class="share-button-twitter" data-lang="en">';
+								$html .='<a target="_blank" href="https://twitter.com/share?text='.rawurlencode(get_the_title()).'&amp;url='.get_permalink().'" class="share-button-twitter" data-lang="en">';
 									switch ( $format ) {
 										case 'image' :
 											$html .= '<img src="'.$icon_url.'" alt="'.$icon_text.'">';
@@ -1562,7 +1562,7 @@ if ( ! function_exists('wc_shortcodes_share_buttons') ) {
 							break;
 						case 'email' :
 							$html .= '<li class="wc-shortcodes-share-button-icon wc-shortcode-share-button-icon-' . $key . $first_class . '">';
-								$html .='<a title="Share by Email" href="mailto:?subject='.urlencode(get_the_title()).'&amp;body='.get_permalink().'">';
+								$html .='<a title="Share by Email" href="mailto:?subject='.rawurlencode(get_the_title()).'&amp;body='.get_permalink().'">';
 									switch ( $format ) {
 										case 'image' :
 											$html .= '<img src="'.$icon_url.'" alt="'.$icon_text.'">';
