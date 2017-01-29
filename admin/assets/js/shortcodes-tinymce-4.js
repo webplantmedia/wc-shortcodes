251103bddf6372171d939f8a55a42c95e08d1dc5
Chris Baldelomar <chris@webplantmedia.com>
2017-01-29 09:29:01 -0600

    organized shortcode menu

--- admin/assets/js/shortcodes-tinymce-4.js
+++ admin/assets/js/shortcodes-tinymce-4.js
@@ -121,13 +121,6 @@
 								}
 							},
 							{
-								text: "Google Map",
-								onclick: function(){
-									var shortcode = '[wc_googlemap title="St. Paul\'s Chapel" location="209 Broadway, New York, NY 10007" zoom="10" height="250" title_on_load="0" class=""]';
-									wcShortcodes( shortcode, editor );
-								}
-							},
-							{
 								text: "Heading",
 								onclick: function(){
 									var shortcode = '[wc_heading type="h1" title="' + wcDummyContent + '" margin_top="" margin_bottom="" text_align="left" font_size="" color="" class="" icon_left="" icon_right="" icon_spacing=""]';
@@ -135,16 +128,40 @@
 								}
 							},
 							{
-								text: "Pricing Table",
+								text: "Highlight",
 								onclick: function(){
-									var shortcode = '[wc_pricing type="primary" plan="Basic" cost="$19.99" per="per month" button_url="http://angiemakes.com" button_text="Sign Up" button_target="self" button_rel="nofollow"]<ul><li>30GB Storage</li><li>512MB Ram</li><li>10 databases</li><li>1,000 Emails</li><li>25GB Bandwidth</li></ul>[/wc_pricing]';
+									var shortcode = '[wc_highlight color="yellow"]' + wcDummyContent + '[/wc_highlight]';
 									wcShortcodes( shortcode, editor );
 								}
 							},
 							{
-								text: "Skillbar",
+								text: "Spacing",
 								onclick: function(){
-									var shortcode = '[wc_skillbar title="' + wcDummyContent + '" percentage="100" color="#6adcfa"]';
+									var shortcode = '[wc_spacing size="40px" class=""][/wc_spacing]';
+									wcShortcodes( shortcode, editor );
+								}
+							},
+							{
+								text: "Clear Floats",
+								onclick: function(){
+									editor.insertContent('[wc_clear_floats]');
+								}
+							},
+							{
+								text: "Inline Code",
+								onclick: function(){
+									editor.insertContent('[wc_code]' + wcDummyContent + '[/wc_code]');
+								}
+							},
+						]
+					},
+					{
+						text: 'Content',
+						menu: [
+							{
+								text: "Pricing Table",
+								onclick: function(){
+									var shortcode = '[wc_pricing type="primary" plan="Basic" cost="$19.99" per="per month" button_url="http://angiemakes.com" button_text="Sign Up" button_target="self" button_rel="nofollow"]<ul><li>30GB Storage</li><li>512MB Ram</li><li>10 databases</li><li>1,000 Emails</li><li>25GB Bandwidth</li></ul>[/wc_pricing]';
 									wcShortcodes( shortcode, editor );
 								}
 							},
@@ -177,22 +194,6 @@
 								}
 							},
 							{
-								text: "Countdown",
-								onclick: function(){
-									var d = new Date();
-									var year = d.getFullYear() + 1;
-									var shortcode = '[wc_countdown date="July 23, '+year+', 6:00:00 PM" format="wdHMs" message="Your Message Here!" labels="Years,Months,Weeks,Days,Hours,Minutes,Seconds" labels1="Year,Month,Week,Day,Hour,Minute,Second"]';
-									wcShortcodes( shortcode, editor );
-								}
-							},
-							{
-								text: "RSVP",
-								onclick: function(){
-									var shortcode = '[wc_rsvp columns="3" align="left" button_align="center"]';
-									wcShortcodes( shortcode, editor );
-								}
-							},
-							{
 								text: "HTML",
 								onclick: function(){
 									var shortcode = '[wc_html name="Custom Field Name"][/wc_html]';
@@ -207,10 +208,9 @@
 								}
 							},
 							{
-								text: "Highlight",
+								text: "Preformatted Text",
 								onclick: function(){
-									var shortcode = '[wc_highlight color="yellow"]' + wcDummyContent + '[/wc_highlight]';
-									wcShortcodes( shortcode, editor );
+									editor.insertContent('[wc_pre color="1" wrap="0" scrollable="1" linenums="0" name="Custom Field Name"]');
 								}
 							}
 						]
@@ -346,25 +346,112 @@
 									var shortcode = '[wc_toggle title="This Is Your Toggle Title" layout="box"]' + wcParagraphContent + '[/wc_toggle]';
 									wcShortcodes( shortcode, editor );
 								}
-							}
-						]
-					},
-					{
-						text: 'Other',
-						menu: [
+							},
 							{
-								text: "Spacing",
+								text: "Google Map",
 								onclick: function(){
-									var shortcode = '[wc_spacing size="40px" class=""][/wc_spacing]';
+									var shortcode = '[wc_googlemap title="St. Paul\'s Chapel" location="209 Broadway, New York, NY 10007" zoom="10" height="250" title_on_load="0" class=""]';
 									wcShortcodes( shortcode, editor );
 								}
 							},
 							{
-								text: "Clear Floats",
+								text: "RSVP",
 								onclick: function(){
-									editor.insertContent('[wc_clear_floats]');
+									var shortcode = '[wc_rsvp columns="3" align="left" button_align="center"]';
+									wcShortcodes( shortcode, editor );
 								}
 							},
+							{
+								text: "Countdown",
+								onclick: function(){
+									var d = new Date();
+									var year = d.getFullYear() + 1;
+									var shortcode = '[wc_countdown date="July 23, '+year+', 6:00:00 PM" format="wdHMs" message="Your Message Here!" labels="Years,Months,Weeks,Days,Hours,Minutes,Seconds" labels1="Year,Month,Week,Day,Hour,Minute,Second"]';
+									wcShortcodes( shortcode, editor );
+								}
+							},
+							{
+								text: "Skillbar",
+								onclick: function(){
+									var shortcode = '[wc_skillbar title="' + wcDummyContent + '" percentage="100" color="#6adcfa"]';
+									wcShortcodes( shortcode, editor );
+								}
+							}
+						]
+					},
+					{
+						text: 'jQuery',
+						menu: [
+							{
+								text: "Accordion",
+								onclick: function(){
+									var shortcode = '[wc_accordion collapse="0" leaveopen="0" layout="box"]<p>[wc_accordion_section title="Section 1"]</p>' + wcParagraphContent + '<p>[/wc_accordion_section]</p><p>[wc_accordion_section title="Section 2"]</p>' + wcDummyParagraphContent + '<p>[/wc_accordion_section]</p>[/wc_accordion]';
+									wcShortcodes( shortcode, editor );
+								}
+							},
+							{
+								text: "Accordion Section",
+								onclick: function(){
+									var shortcode = '[wc_accordion_section title="New Accordion Section"]<p>' + wcParagraphContent + '</p>[/wc_accordion_section]';
+									wcShortcodes( shortcode, editor );
+								}
+							},
+							{
+								text: "Tabs",
+								onclick: function(){
+									var shortcode = '[wc_tabgroup layout="box" class=""]<p>[wc_tab title="First Tab"]</p>'+wcParagraphContent+'<p>[/wc_tab]</p><p>[wc_tab title="Second Tab"]</p>'+wcDummyParagraphContent+'<p>[/wc_tab]</p>[/wc_tabgroup]';
+									wcShortcodes( shortcode, editor );
+								}
+							},
+							{
+								text: "Tab Section",
+								onclick: function(){
+									var shortcode = '[wc_tab title="New Tab Section"]<p>'+wcParagraphContent+'</p>[/wc_tab]';
+									wcShortcodes( shortcode, editor );
+								}
+							},
+							{
+								text: "Toggle",
+								onclick: function(){
+									var shortcode = '[wc_toggle title="This Is Your Toggle Title" layout="box"]' + wcParagraphContent + '[/wc_toggle]';
+									wcShortcodes( shortcode, editor );
+								}
+							},
+							{
+								text: "Google Map",
+								onclick: function(){
+									var shortcode = '[wc_googlemap title="St. Paul\'s Chapel" location="209 Broadway, New York, NY 10007" zoom="10" height="250" title_on_load="0" class=""]';
+									wcShortcodes( shortcode, editor );
+								}
+							},
+							{
+								text: "RSVP",
+								onclick: function(){
+									var shortcode = '[wc_rsvp columns="3" align="left" button_align="center"]';
+									wcShortcodes( shortcode, editor );
+								}
+							},
+							{
+								text: "Countdown",
+								onclick: function(){
+									var d = new Date();
+									var year = d.getFullYear() + 1;
+									var shortcode = '[wc_countdown date="July 23, '+year+', 6:00:00 PM" format="wdHMs" message="Your Message Here!" labels="Years,Months,Weeks,Days,Hours,Minutes,Seconds" labels1="Year,Month,Week,Day,Hour,Minute,Second"]';
+									wcShortcodes( shortcode, editor );
+								}
+							},
+							{
+								text: "Skillbar",
+								onclick: function(){
+									var shortcode = '[wc_skillbar title="' + wcDummyContent + '" percentage="100" color="#6adcfa"]';
+									wcShortcodes( shortcode, editor );
+								}
+							}
+						]
+					},
+					{
+						text: 'Wrappers',
+						menu: [
 							{
 								text: "Center Content",
 								onclick: function(){
@@ -377,18 +464,6 @@
 									editor.insertContent('[wc_fullwidth selector=""]' + wcParagraphContent + '[/wc_fullwidth]');
 								}
 							},
-							{
-								text: "Code",
-								onclick: function(){
-									editor.insertContent('[wc_code]' + wcDummyContent + '[/wc_code]');
-								}
-							},
-							{
-								text: "Pre",
-								onclick: function(){
-									editor.insertContent('[wc_pre color="1" wrap="0" scrollable="1" linenums="0" name="Custom Field Name"]');
-								}
-							}
 						]
 					},
 					{
@@ -396,7 +471,7 @@
 						onclick: function(){
 							wcShortcodes( mceSelected, editor );
 						}
-					}
+					},
 				]
 			}
 		});
